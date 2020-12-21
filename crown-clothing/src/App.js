5b63b3d2431367a266b1c33fd931f9ef4bf85c9d
furqanalimir <furkaanali89@gmail.com>
2020-12-21 19:03:54 +0530

    updated menu-item to navigate

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage.component';
 
-
+const HatsPage = () =>(
+  <div>
+    <h1> HatsPage </h1>
+  </div>
+)
 function App() {
   return (
     <div>
-    <HomePage>
-
-    </HomePage>
+  <switch > 
+    <Route exact path="/" component={HomePage} />
+    <Route  path="/shop/hats" component={HatsPage} />
+  </switch>
     </div>
   );
 }
--- crown-clothing/src/components/menu-item/menu-item.component.jsx
+++ crown-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import '../menu-item/menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) =>(    //destructured item is passsed
-    <div className= {`${size} menu-item`} >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) =>(    //destructured item is passsed, history and match is builtin variable
+    <div className= {`${size} menu-item`}  
+    onClick={ ()=> history.push(`${match.url}${linkUrl}`)} >
 
-        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}} >
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}} />
 
         <div className="content">
             <h1 className="title"> { title.toUpperCase() } </h1>   {/*this is same as props.title if props was passed in MenuItem  */}
             <span className="subtitle"> SHOP NOW</span>
         </div>
-        
-        </div>
     </div>
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
