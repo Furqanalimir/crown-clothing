06ad385f382f542551c4d89efd51c8f48f59738f
furqanalimir <furkaanali89@gmail.com>
2020-12-26 23:20:53 +0530

    integrated firebase into application, created signin page with signin with google

--- crown-clothing/src/components/sign-in/sign-in.component.jsx
+++ crown-clothing/src/components/sign-in/sign-in.component.jsx
@@ -50,10 +50,12 @@ class SignIn extends Component {
                 label='password'
                 placeholder='password'
                 required />
-                
-                <CustomButton type="submit" > Sign in</CustomButton> {'  '}
-                <CustomButton onClick={SignInWithGoogle} > Sign in with Google</CustomButton>
+                <div className="buttons">
 
+                <CustomButton type="submit" > Sign in</CustomButton> 
+                <CustomButton onClick={SignInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
+
+                </div>
             </form>
             </div>
         )
